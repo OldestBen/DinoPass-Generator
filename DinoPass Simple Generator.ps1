2f4810b86c87d9a06858bff1e370d91eb4b2edab
OldestBen <61541898+OldestBen@users.noreply.github.com>
2024-07-22 22:06:31 +0100

    Add files via upload

---
+++ DinoPass Simple Generator.ps1
@@ -0,0 +1,19 @@
+﻿# Define the number of passwords to generate
+$numberOfPasswords = 222
+# Create an empty array to hold the passwords
+$passwords = @()
+
+# Loop to generate each password
+for ($i = 0; $i -lt $numberOfPasswords; $i++) {
+    # Call DinoPass to get a simple password
+    $response = Invoke-WebRequest -Uri "https://www.dinopass.com/password/simple" | Select-Object -ExpandProperty content
+    # Create a custom object with a Password property
+    $passwordObject = New-Object PSObject -Property @{
+        Password = $response
+    }
+    # Add the password object to the array
+    $passwords += $passwordObject
+}
+
+# Export the passwords to a CSV file
+$passwords | Export-Csv -Path "C:\simplepasswords.csv" -NoTypeInformation
---
+++ DinoPass Strong Generator.ps1
@@ -0,0 +1,19 @@
+# Define the number of passwords to generate
+$numberOfPasswords = 222
+# Create an empty array to hold the passwords
+$passwords = @()
+
+# Loop to generate each password
+for ($i = 0; $i -lt $numberOfPasswords; $i++) {
+    # Call DinoPass to get a simple password
+    $response = Invoke-WebRequest -Uri "https://www.dinopass.com/password/strong" | Select-Object -ExpandProperty content
+    # Create a custom object with a Password property
+    $passwordObject = New-Object PSObject -Property @{
+        Password = $response
+    }
+    # Add the password object to the array
+    $passwords += $passwordObject
+}
+
+# Export the passwords to a CSV file
+$passwords | Export-Csv -Path "C:\strongpasswords.csv" -NoTypeInformation
